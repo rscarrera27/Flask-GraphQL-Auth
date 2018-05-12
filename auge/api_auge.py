05d0d761c3882997e841c173d54592a33e8f8e23
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-05-12 22:33:33 +0900

    IMPLEMENT: make_signature

--- auge/api_auge.py
+++ auge/api_auge.py
@@ -6,19 +6,35 @@ default_header = {
   "typ": "JWT"
 }
 
+
 def make_signature(header, payload, secret):
+    header = utils.base64url_encode(header)
+    payload = utils.base64url_encode(payload)
+
+    segement = header+b'.'+payload
+
+    HS256 = algorithms.get_default_algorithms()['HS256']
+    key = HS256.prepare_key(secret)
+    signature = HS256.sign(segement, key)
+
+    return signature
+
 
 def create_token(identity, exp, key, **claims):
     pass
 
+
 def create_refresh_token(identity, exp, key, **user):
     pass
+
 
 def create_access_token(identity, exp, key, **user):
     pass
 
+
 def decode_token(token, secret):
     pass
+
 
 def get_token(token, secret):
     pass
