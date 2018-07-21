7afd24ed75635212931be3b1c0d432ac0c590530
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-07-21 15:39:40 +0900

    NEW VERSION

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-GraphQL-Auth',
-    version=0.3,
+    version=0.5,
     url="https://github.com/devArtoria/flask-graphql-auth",
     license='MIT',
     author='Lewis "devArtoria" Kim',
