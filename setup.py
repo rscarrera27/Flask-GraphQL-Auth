3e82b62de239dbc040bbd4b550315cce1df49fe5
Seonghyeon Kim <artoria@artoria.us>
2019-05-14 08:20:46 +0900

    Update project setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='Flask-GraphQL-Auth',
-    version='1.1.1',
+    version='1.2.0',
     url="https://github.com/devArtoria/flask-graphql-auth",
     license='MIT',
-    author='devArtoria',
+    author='NovemberOscar',
     author_email='artoria@artoria.us',
     keywords=['jwt', 'auth', 'graphql'],
     description="JWT library for Flask-GraphQL",
@@ -13,6 +13,7 @@ setup(
     install_requires=[
         'PyJWT',
         'Flask_GraphQL',
-        'graphene'
+        'graphene',
+        'flask',
     ]
 )
